03db124ee46b32c8868d6babece846afe3a005eb
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-10-09 21:43:54 +0200

    Another fix.

--- server/actors.js
+++ server/actors.js
@@ -490,7 +490,7 @@ ActorAsteroid.onCreate = function(data) {
     }
     this.polygon = new polygon.Polygon2D(this.x, this.y, this.r,
                                          ActorAsteroid.points[this.type - 1],
-                                         this.type < 4 ? 3 : 5);
+                                         this.type < 4 ? 2.5 : 5);
     
     var found = false;
     var tries = 0;
